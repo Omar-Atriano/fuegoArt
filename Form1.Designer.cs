f078086cfac087c910ac7f23ce6de8a5e8a8b0a5
Omar-Atriano <omar.atrianova@udlap.mx>
2023-02-22 22:51:08 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace particulas2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.Location = new System.Drawing.Point(0, 0);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(797, 444);
+            this.PCT_CANVAS.TabIndex = 0;
+            this.PCT_CANVAS.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pelota.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace particulas2
+{
+    public class Pelota
+    {
+        int index;
+        Size space;
+        public Color c;
+        // Variables de posición
+        public float x;
+        public float y;
+        //static Random rand = new Random();
+
+        // Variables de velocidad
+        private float vx;
+        private float vy;
+        const float gravity = 0.05f;
+
+        // Variable de radio
+        public float radio;
+
+        //Variables de edad
+        private int age;
+        public bool IsDead ;
+
+        // Constructor
+        public Pelota(Random rand, Size size, int index)
+        {
+            this.radio =1;
+            this.x = rand.Next(280, 320);
+            this.y = rand.Next(150, 200);
+            c = Color.FromArgb(255,231, 105, 38);
+            // Velocidades iniciales
+            this.vx = rand.Next(-2, 3);
+            this.vy = rand.Next(-2, 3);
+
+            //edades
+            age = 200;
+            this.IsDead = false;
+
+            this.index = index;
+            space = size;
+        }
+
+        // Método para actualizar la posición de la pelota en función de su velocidad
+        public void Update(float deltaTime, List<Pelota> balls)
+        {
+            for (int b = index + 1; b < balls.Count; b++)
+            {
+                Collision(balls[b]);
+            }
+
+            if ((x - radio) <= 0 || (x + radio) >= space.Width)
+            {
+                if (x - radio <= 0)
+                    x = radio + 3;
+                else
+                    x = space.Width - radio - 3;
+
+                vx *= -.005f;
+                vy *= .075f;
+            }
+
+            if ((y - radio) <= 0 || (y + radio) >= space.Height)
+            {
+                if (y - radio <= 0)
+                    y = radio + 3;
+                else
+                    y = space.Height - radio - 3;
+
+                vx = 0;
+                vy = 0;
+            }
+
+
+            age -= 1;
+            if (age <= 0)
+            {
+                IsDead = true;
+            }
+
+            vy += gravity;
+
+
+            int alpha = (int)Math.Min(255, 255 * this.age / 300);
+            if (alpha <= 0) alpha = 0;
+            this.c = Color.FromArgb(alpha, c.R, c.G, c.B);
+
+            this.x += this.vx * 2;
+            this.y += this.vy * 2;
+
+        }
+
+
+
+        // Método para manejar colisiones entre pelotas
+        public void Collision(Pelota otraPelota)
+        {
+            float distancia = (float)Math.Sqrt(Math.Pow((otraPelota.x - this.x), 2) + Math.Pow((otraPelota.y - this.y), 2));
+
+            if (distancia < (this.radio + otraPelota.radio))//ESTO SIGNIFICA COLISIÓN...
+            {
+                // Calculamos las velocidades finales de cada pelota en función de su masa y velocidad inicial
+                float masaTotal = this.radio + otraPelota.radio;
+                float masaRelativa = this.radio / masaTotal;
+
+                float v1fx = this.vx - masaRelativa * (this.vx - otraPelota.vx) / 100;
+                float v1fy = this.vy - masaRelativa * (this.vy - otraPelota.vy) / 100;
+
+                float v2fx = otraPelota.vx - masaRelativa * (otraPelota.vx - this.vx) / 100;
+                float v2fy = otraPelota.vy - masaRelativa * (otraPelota.vy - this.vy) / 100;
+
+                // Actualizamos las velocidades de las pelotas
+                this.vx = v1fx;     // -----AQUI CAMBIAMOS EL ANGULO---------
+                this.vy = v1fy;     // -----AQUI CAMBIAMOS EL ANGULO--------------
+
+                otraPelota.vx = v2fx;//-----AQUI CAMBIAMOS EL ANGULO----------------------
+                otraPelota.vy = v2fy;//-----AQUI CAMBIAMOS EL ANGULO----------------------
+
+                // Movemos las pelotas para evitar que se superpongan
+                float distanciaOverlap = (this.radio + otraPelota.radio) - distancia;
+                float dx = (this.x - otraPelota.x) / distancia;
+                float dy = (this.y - otraPelota.y) / distancia;
+
+                this.x += dx * distanciaOverlap / 2f;
+                this.y += dy * distanciaOverlap / 2f;
+
+                otraPelota.x -= dx * distanciaOverlap / 2f;
+                otraPelota.y -= dy * distanciaOverlap / 2f;
+            }
+        }
+
+    }
+}
